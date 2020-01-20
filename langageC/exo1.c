8daeb2ad2659695aa3ae85e7c145e7a3a7ca35e0
Aiden P-L <59953339+Aidenosaur@users.noreply.github.com>
2020-01-21 00:56:40 +0100

    Exercice 2
    Devoir enchaîner des coups jusqu'à ce que le monstre meurt.

---
+++ langageC/exo1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){	
+	int pvJ1;
+	int pvMonstre;
+	int attaque;
+	
+	pvJ1 = 100;
+	pvMonstre = 100;
+	attaque = 10;
+	
+	//Voir si laisser choisir nom joueur est possible comme scanf mais pour caractère.
+	
+	//Début du combat et affichage des PV.
+	
+	printf("Un monstre arrive ! Defendez vous ! \n PV Joueur : %d \n PV Monstre : %d \n", pvJ1, pvMonstre);
+	
+	//Attaquer l'ennemi.
+	
+	printf("Vous devez vite attaquer ! \nVous sortez votre hache et assenez un coup virulent!! \nLe monstre perd 10 PV !\n");
+	
+	pvMonstre=pvMonstre-attaque;
+	
+	printf(" Monstre = %d PV restant, \n Joueur = %d PV restant \n", pvMonstre, pvJ1);
+
+//EXO2 : Attaquer jusqu'à ce que pv monstre=0
+
+while(pvMonstre>0){
+	printf("Vous enchainez les coups!! \n");
+	
+	pvMonstre=pvMonstre-attaque;
+	
+	printf(" Monstre = %d PV restant, \n Joueur = %d PV restant \n", pvMonstre, pvJ1);
+}
+printf("Le Monstre est KO ! Vous avez gagne le combat !!");
+}
+
+
+	
